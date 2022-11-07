0b3d10e970dc88da1c2e64db220d4062dd1b3006
Roman Ishchuk <01165722@pw.edu.pl>
2022-11-07 20:22:50 +0100

    Update Prolog/main.pl

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -98,10 +98,12 @@ take(X) :-
         assert(holding(X)),
         format('You have picked the ~s\n', [X]), !.
 
-take(fridge) :- 
+take(X) :- 
+        (X = fridge; X = wardrobe),
         player_at(Place),
-        at(fridge, Place),
-        writeln('I cant take a fridge It''s too heavy'), !.  
+        at(X, Place),
+        format('I cant take a ~s\n', [X]),
+        writeln('It''s too heavy'), !.  
 
 take(_) :-
         writeln('I don''t see it here.'), !.
